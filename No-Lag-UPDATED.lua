3f33e796e36bccda80ddecf04e85ae6c515adb6d
AtlasO <jerichoplayz3007@gmail.com>
2025-06-15 20:20:34 +0800

    Update No-Lag-UPDATED.lua

--- No-Lag-UPDATED.lua
+++ No-Lag-UPDATED.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/RobloxBestScriptHubs/e9dd45b2a1e2a2bfef389090c67828e1/raw/script.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/RobloxBestScriptHubs/61eaf5088a9258f8820fd39be498d524/raw/script.lua"))()
